df1bc8acd1fd6e0910018d1dd8cb4198c490b4f7
to.my.trociny <to.my.trociny@4e482700-5941-0410-bbc7-7991b37b8a4a>
2008-05-11 12:22:16 +0000

    some cleanup

--- mibmem.c
+++ mibmem.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: mibmem.c,v 1.4.2.1 2008/02/02 18:38:33 mikolaj Exp $
+ * $Id: mibmem.c,v 1.4.2.2 2008/05/11 12:10:28 mikolaj Exp $
  *
  */
 
