1c3f7d792cfbd38b415eca0566abc250ecac3227
to.my.trociny <to.my.trociny@4e482700-5941-0410-bbc7-7991b37b8a4a>
2009-05-09 16:22:02 +0000

    bsnmp-ucd-0.2.2 release

---
+++ tags/bsnmp-ucd-0.2.2/mibext.c
@@ -0,0 +1,532 @@
+/*
+ * Copyright (c) 2007 Mikolaj Golub
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: mibext.c,v 1.14 2009/05/04 13:27:07 mikolaj Exp $
+ *
+ */
+
+#include <syslog.h>
+#include <sys/queue.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <paths.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include "snmp_ucd.h"
+
+/*
+ * mibext structures and functions
+ */
+
+struct mibext {
+	TAILQ_ENTRY(mibext)	link;
+	int32_t			index;
+	u_char			*names;
+	u_char			*command;
+	int32_t			result;
+	u_char			output[UCDMAXLEN];
+	int32_t			errFix;
+	u_char			*errFixCmd;
+	int 			_fd[2];
+	int			_is_running;
+	uint64_t		_ticks;
+	uint64_t		_fix_ticks;
+};
+
+struct ext_msg {
+	int32_t		result;
+	u_char		output[UCDMAXLEN];
+};
+
+TAILQ_HEAD(mibext_list, mibext);
+
+static struct mibext_list mibext_list = TAILQ_HEAD_INITIALIZER(mibext_list);
+
+static struct mibext *
+find_ext (int32_t idx)
+{
+	struct mibext *extp;
+	TAILQ_FOREACH(extp, &mibext_list, link)
+		if (extp->index == idx)
+			return (extp);
+	return (NULL);
+}
+
+static struct mibext *
+first_mibext(void)
+{
+	return (TAILQ_FIRST(&mibext_list));
+}
+
+#if 0 /* not used now */
+static struct mibext *
+next_mibext(const struct mibext *extp)
+{
+	return (TAILQ_NEXT(extp, link));
+}
+#endif
+
+/* handle timeouts when executing commands */
+
+static void
+extcmd_sighandler(int sig __unused)
+{
+	_exit(127);
+}
+
+
+/* run commands and collect results of programs that have already finished */
+
+void
+run_extCommands(void* arg __unused)
+{
+	struct mibext	*extp;
+
+	uint64_t	current;
+	
+	current = get_ticks();
+
+	/* run commads which are ready for running */
+	
+	TAILQ_FOREACH(extp, &mibext_list, link) {
+		int	status;
+		pid_t	pid;
+		
+		if (!extp->command)
+			continue; /* no command specified */
+
+		if (extp->_is_running)
+			continue; /* command has already been running */
+
+		if ((current - extp->_ticks) < EXT_UPDATE_INTERVAL)
+			continue; /* run command only if EXT_UPDATE_INTERVAL passes after last run */
+		
+		/* create a pipe */
+		if (pipe(extp->_fd) != 0) {
+			syslog(LOG_ERR, "failed to pipe: %s: %m", __func__);
+			continue;
+		}
+
+		/* make the pipe non-blocking */
+		fcntl(extp->_fd[0],F_SETFL,O_NONBLOCK);
+		fcntl(extp->_fd[1],F_SETFL,O_NONBLOCK);
+
+		pid = fork();
+
+		/* execute the command in the child process */
+		if (pid==0) {
+			int fd;
+			
+			/* close all descriptors except stdio and pipe for reading */
+			for (fd = 3; fd < extp->_fd[1]; fd++)
+				close(fd);
+
+			/* fork again */
+			if ((pid = fork()) < 0) {
+				syslog(LOG_ERR, "Can't fork: %s: %m", __func__);
+				_exit(127);
+			}
+
+			if (pid == 0) { /* grandchild */
+				struct ext_msg	msg;
+				char	null[UCDMAXLEN];
+				FILE	*fp;
+				
+				msg.output[0] = '\0';
+				
+				/* become process group leader */
+				setpgid(0, 0);
+
+				/* trap commands that timeout */
+				signal(SIGALRM, extcmd_sighandler);
+ 				alarm(EXT_TIMEOUT);
+				
+				/* run the command */
+				if ((fp = popen((char*) extp->command, "r")) == NULL) {
+					syslog(LOG_ERR, "popen failed: %s: %m", __func__);
+					/* send empty line to parent and exit*/
+					msg.result = 127;
+					write(extp->_fd[1], (char*) &msg, sizeof(msg));
+					_exit(127);
+				}
+
+				/* read first line to output buffer*/
+				if (fgets((char*) msg.output, sizeof(msg.output), fp) != NULL) { /* we have some output */
+					int	end;
+					
+					/* chop 'end of line' */
+					end = strlen((char*) msg.output) - 1;
+					if ((end >= 0) && (msg.output[end] == '\n'))
+						msg.output[end] = '\0';
+
+					/* just skip other lines */
+					while (fgets(null, sizeof(null), fp) != NULL);
+				
+				}
+				
+				status = pclose(fp);
+				msg.result = WEXITSTATUS(status);
+				/* send result to parent*/
+				write(extp->_fd[1], (char*) &msg, sizeof(msg));
+				close(extp->_fd[1]);
+				_exit(msg.result);
+				
+			} else { /* parent of grandchild */
+
+				_exit(0);
+			}
+		}
+
+		/* parent (bsnmpd process) */
+		
+		if (pid < 0) {
+			syslog(LOG_ERR, "Can't fork: %s: %m", __func__);
+			close(extp->_fd[0]);
+			close(extp->_fd[1]);
+			continue;
+		}
+		
+		/* close pipe for writing */
+		close(extp->_fd[1]);
+
+		/* wait for child */
+		for (;;) {
+			pid_t res;
+			res = waitpid(pid, &status, 0);
+			if (res == -1 && errno == EINTR)
+				continue;
+			
+			if (res <= 0) {
+				syslog(LOG_ERR, "waitpid failed: %s: %m", __func__);
+				break;
+			} else {
+				/* get the exit code returned from the program */
+				status = WEXITSTATUS(status);
+			}
+			
+			if ((res <= 0) || status) {
+				/* something wrong with running program */
+				/* consider it as program has finished abnormaly */
+
+				/* save time of program finishing */
+				extp->_ticks = get_ticks();
+				
+				/* close pipe for reading */
+				close(extp->_fd[0]);
+
+				/* fill extp data */
+				extp->result = 127;
+				extp->output[0] = '\0';
+				extp->_is_running = 0;
+			} else { /*  program is runnng */
+				extp->_is_running = 1;
+			}
+			break;
+		}
+	}
+
+	/* collect data of finished commands */
+	
+	TAILQ_FOREACH(extp, &mibext_list, link) {
+		struct ext_msg	msg;
+		int	n;
+
+		if (!extp->_is_running)
+			break; /* programm is not running */
+
+		for (;;) {
+			n = read(extp->_fd[0], (char*) &msg, sizeof(msg));
+			
+			if (n == -1 && errno == EINTR)
+				continue;	/* interrupted; try again */
+			
+			if (n == -1 && errno == EAGAIN)
+				break;		/* no data this time */
+			
+			if (n != sizeof(msg)) {
+				/* read returned something wrong */
+				/* mark command as abnormally finished */
+				extp->result = 127;
+				strncpy((char*) extp->output, "Exited abnormally!", sizeof(extp->output) - 1);
+			} else {
+				extp->result = msg.result;
+				strncpy((char*) extp->output, (char*) msg.output, sizeof(extp->output) - 1);
+			}
+			
+			extp->_is_running = 0;
+			
+			close(extp->_fd[0]);
+			
+			/* save time of program finishing */
+			extp->_ticks = get_ticks();
+
+			break;
+		}
+	}
+}
+
+/* get max fd opened */
+
+static int
+get_fdmax(void) {
+	int fd;
+	/* FIXME: may be there is more simple way to find out max fd */
+	fd = open(_PATH_DEVNULL, O_RDONLY);
+	if (fd < 0) {
+		syslog(LOG_ERR, "Can't open %s: %s: %m", _PATH_DEVNULL, __func__);
+		return -1;
+	}
+	close(fd);
+	return fd-1;
+}
+
+/* run fix commands */
+
+void
+run_extFixCmds(void* arg __unused)
+{
+	struct mibext	*extp;
+
+	uint64_t	current;
+	
+	current = get_ticks();
+
+	/* run commads if needed */
+	
+	TAILQ_FOREACH(extp, &mibext_list, link) {
+		pid_t	pid;
+		int	status;
+		
+		if (!extp->errFix)
+			continue;	/* no fix */
+
+		if (!extp->errFixCmd)
+			continue;	/* no command specified */
+
+		if (extp->result == 0)
+			continue;	/* checked command exited normaly, no need for fix*/
+
+		if ((current - extp->_fix_ticks) < EXT_UPDATE_INTERVAL)
+			continue; /* run command only if EXT_UPDATE_INTERVAL passes after last run */
+
+		pid = fork();
+		
+		/* execute the command in the child process */
+		if (pid==0) {
+
+			int	fd, fdmax;
+			
+			/* close all descriptors except stdio */
+			fdmax = get_fdmax();
+			for (fd = 3; fd <= fdmax; fd++)
+				close(fd);
+
+			/* fork again */
+			
+			if ((pid = fork()) < 0) {
+				syslog(LOG_ERR, "Can't fork: %s: %m", __func__);
+				_exit(127);
+			}
+
+			if (pid == 0) { /* grandchild */
+				
+				/* become process group leader */
+				setpgid(0, 0);
+
+				/* trap commands that timeout */
+				signal(SIGALRM, extcmd_sighandler);
+				alarm(EXT_TIMEOUT);
+
+				/* run the command */
+				if ((status = system((char*) extp->errFixCmd)) != 0)
+					syslog(LOG_WARNING, "command `%s' has retuned status %d", extp->errFixCmd, WEXITSTATUS(status));
+				_exit(WEXITSTATUS(status));
+				
+			}
+
+			/* parent of grandchild */
+			_exit(0);
+		}
+
+		/* parent */
+
+		if (pid < 0)
+			syslog(LOG_ERR, "Can't fork: %s: %m", __func__);
+
+		/* wait for child */
+		for (;;) {
+			pid_t res;
+			res = waitpid(pid, &status, 0);
+			if (res == -1 && errno == EINTR)
+				continue;
+			if (res <= 0)
+				syslog(LOG_ERR, "failed to waitpid: %s: %m", __func__);
+			break;
+		}		
+		extp->_fix_ticks = get_ticks();
+	}
+}
+
+int
+op_extTable(struct snmp_context * context __unused, struct snmp_value * value, 
+	u_int sub, u_int iidx __unused, enum snmp_op op)
+{
+	int ret;
+	struct mibext *extp = NULL;
+	asn_subid_t which = value->var.subs[sub - 1];
+
+	switch (op) {
+
+		case SNMP_OP_GETNEXT:
+			if ((extp = NEXT_OBJECT_INT(&mibext_list, &value->var, sub)) == NULL)
+				return (SNMP_ERR_NOSUCHNAME);
+			value->var.len = sub + 1;
+			value->var.subs[sub] = extp->index;
+			break;
+
+		case SNMP_OP_GET:
+			if (value->var.len - sub != 1)
+				return (SNMP_ERR_NOSUCHNAME);
+			if ((extp = find_ext(value->var.subs[sub])) == NULL)
+				return (SNMP_ERR_NOSUCHNAME);
+			break;
+
+		case SNMP_OP_SET:
+			switch (which) {
+				case LEAF_extNames:
+					if ((extp = find_ext(value->var.subs[sub])) == NULL) {
+						if ((extp = malloc(sizeof(*extp))) == NULL) {
+							syslog(LOG_ERR, "failed to malloc: %s: %m", __func__);
+							return (SNMP_ERR_NOT_WRITEABLE);
+						}
+						memset(extp, 0, sizeof(*extp));
+						extp->index = value->var.subs[sub];
+						INSERT_OBJECT_INT(extp, &mibext_list);
+					} else {
+						/* we have already had some command defined  under this index.*/
+						/* check if the command is run to close our end of pipe */
+						if (extp->_is_running) {
+							close(extp->_fd[0]);
+							extp->_is_running = 0;
+						}
+					}
+					return  string_save(value, context, -1, &extp->names);
+
+				case LEAF_extCommand:
+					if ((extp = find_ext(value->var.subs[sub])) == NULL)
+						return (SNMP_ERR_NOT_WRITEABLE);
+					return  string_save(value, context, -1, &extp->command);
+
+				case LEAF_extErrFix:
+					if ((extp = find_ext(value->var.subs[sub])) == NULL)
+						return (SNMP_ERR_NOT_WRITEABLE);
+					extp->errFix = value->v.integer;
+					return SNMP_ERR_NOERROR;
+
+				case LEAF_extErrFixCmd:
+					if ((extp = find_ext(value->var.subs[sub])) == NULL)
+						return (SNMP_ERR_NOT_WRITEABLE);
+					return  string_save(value, context, -1, &extp->errFixCmd);
+
+				default:
+					break;
+			}
+			return (SNMP_ERR_NOT_WRITEABLE);
+    
+		case SNMP_OP_ROLLBACK:
+		case SNMP_OP_COMMIT:
+			return (SNMP_ERR_NOERROR);
+    
+		default:
+			return (SNMP_ERR_RES_UNAVAIL);
+	}
+
+	ret = SNMP_ERR_NOERROR;
+
+	switch (which) {
+		
+		case LEAF_extIndex:
+			value->v.integer = extp->index;
+			break;
+    		
+		case LEAF_extNames:
+			ret = string_get(value, extp->names, -1);
+			break;
+
+		case LEAF_extCommand:
+			ret = string_get(value, extp->command, -1);
+			break;
+
+		case LEAF_extResult:
+			value->v.integer = extp->result;
+			break;
+
+		case LEAF_extOutput:
+			ret = string_get(value, extp->output, -1);
+			break;
+
+		case LEAF_extErrFix:
+			value->v.integer = extp->errFix;
+			break;
+
+		case LEAF_extErrFixCmd:
+			ret = string_get(value, extp->errFixCmd, -1);
+			break;
+
+		default:
+			ret = SNMP_ERR_RES_UNAVAIL;
+			break;
+	}
+
+	return (ret);
+};
+
+/* free mibext list */
+static void
+mibext_free (void)
+{
+	struct mibext *extp = NULL;
+	while ((extp = first_mibext()) != NULL) {
+		TAILQ_REMOVE (&mibext_list, extp, link);
+		free(extp->names);
+		free(extp->command);
+		free(extp->errFixCmd);
+		free (extp);
+	}
+}
+
+void
+mibext_fini (void)
+{
+	mibext_free();
+}
---
+++ tags/bsnmp-ucd-0.2.2/mibss.c
@@ -0,0 +1,312 @@
+/*
+ * Copyright (c) 2007 Mikolaj Golub
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: mibss.c,v 1.6 2008/02/01 21:55:27 mikolaj Exp $
+ *
+ */
+
+#include <syslog.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <paths.h>
+
+#include "snmp_ucd.h"
+
+/*
+ * mibmemory structures and functions
+ */
+
+struct mibss {
+	int32_t		index;		/* is always 1 */
+	const u_char 	*errorName;	/* is always "systemStats" */
+	int32_t		swapIn;
+	int32_t		swapOut;
+	int32_t		sysInterrupts;
+	int32_t		sysContext;
+	int32_t		cpuUser;
+	int32_t		cpuSystem;
+	int32_t		cpuIdle;
+	uint32_t	cpuRawUser;
+	uint32_t	cpuRawNice;
+	uint32_t	cpuRawSystem;
+	uint32_t	cpuRawIdle;
+	uint32_t	cpuRawWait;
+	uint32_t	cpuRawKernel;
+	uint32_t	cpuRawInterrupt;
+	uint32_t	rawInterrupts;
+	uint32_t	rawContexts;
+	uint32_t	cpuRawSoftIRQ;
+	uint32_t	rawSwapIn;
+	uint32_t	rawSwapOut;
+};
+
+static struct mibss mibss;
+
+static int pagesize;	/* initialized in init_mibss() */
+
+#define pagetok(size) ((size) * (pagesize >> 10))
+
+/*
+ *  (This has been stolen from BSD top utility)
+ *
+ *  percentages(cnt, out, new, old, diffs) - calculate percentage change
+ *	between array "old" and "new", putting the percentages i "out".
+ *	"cnt" is size of each array and "diffs" is used for scratch space.
+ *	The array "old" is updated on each call.
+ *	The routine assumes modulo arithmetic.  This function is especially
+ *	useful on BSD mchines for calculating cpu state percentages.
+ */
+
+static long 
+percentages(int cnt, int *out, register long *new, register long *old, long *diffs)
+{
+    register int i;
+    register long change;
+    register long total_change;
+    register long *dp;
+    long half_total;
+
+    /* initialization */
+    total_change = 0;
+    dp = diffs;
+
+    /* calculate changes for each state and the overall change */
+    for (i = 0; i < cnt; i++)
+    {
+	if ((change = *new - *old) < 0)
+	{
+	    /* this only happens when the counter wraps */
+	    change = (int)
+		((unsigned long)*new-(unsigned long)*old);
+	}
+	total_change += (*dp++ = change);
+	*old++ = *new++;
+    }
+
+    /* avoid divide by zero potential */
+    if (total_change == 0)
+    {
+	total_change = 1;
+    }
+
+    /* calculate percentages based on overall change, rounding up */
+    half_total = total_change / 2l;
+
+    /* Do not divide by 0. Causes Floating point exception */
+    if(total_change) {
+        for (i = 0; i < cnt; i++)
+        {
+          *out++ = (int)((*diffs++ * 1000 + half_total) / total_change);
+        }
+    }
+
+    /* return the total in case the caller wants to use it */
+    return(total_change);
+}
+
+void
+get_ss_data(void* arg  __unused)
+{
+	static int32_t oswappgsin = -1;
+	static int32_t oswappgsout = -1;
+	static int32_t ointr = -1;
+	static int32_t oswtch = -1;
+	static uint64_t last_update = 0;
+	uint64_t current;
+
+	/* for cpu stats */
+	static int	cpu_states[CPUSTATES];
+	static long	cp_time[CPUSTATES];
+	static long	cp_old[CPUSTATES];
+	static long	cp_diff[CPUSTATES];
+	size_t		cp_time_size = sizeof(cp_time);
+
+	u_long		val;
+
+	sysctlval("vm.stats.vm.v_swappgsin", &val);
+	mibss.rawSwapIn = (uint32_t) val;
+	sysctlval("vm.stats.vm.v_swappgsout", &val);
+	mibss.rawSwapOut = (uint32_t) val;
+	sysctlval("vm.stats.sys.v_intr", &val);
+	mibss.rawInterrupts = (uint32_t) val;
+	sysctlval("vm.stats.sys.v_swtch", &val);
+	mibss.rawContexts = (uint32_t) val;
+	sysctlval("vm.stats.sys.v_soft", &val);
+	mibss.cpuRawSoftIRQ = (uint32_t) val;
+
+	current = get_ticks();
+	if (current > last_update) {
+		if (oswappgsin < 0) { /* first interval */
+			mibss.swapIn  = 0;
+			mibss.swapOut = 0;
+			mibss.sysInterrupts = 0;
+			mibss.sysContext = 0;
+		} else {
+			mibss.swapIn = pagetok(((mibss.rawSwapIn - oswappgsin))) / (current-last_update);
+			mibss.swapOut = pagetok(((mibss.rawSwapOut - oswappgsout))) / (current-last_update);
+			mibss.sysInterrupts = (mibss.rawInterrupts - ointr) / (current-last_update);
+			mibss.sysContext = (mibss.rawContexts - oswtch) / (current-last_update);
+		}
+		oswappgsin  = mibss.rawSwapIn;
+		oswappgsout = mibss.rawSwapOut;
+		ointr = mibss.rawInterrupts;
+		oswtch = mibss.rawContexts;
+
+		last_update = current;
+	}
+
+	if (sysctlbyname("kern.cp_time", &cp_time, &cp_time_size, NULL, 0) < 0)
+		syslog(LOG_ERR, "sysctl failed: %s: %m", __func__);
+
+	/* convert cp_time counts to percentages * 10 */
+	percentages(CPUSTATES, cpu_states, cp_time, cp_old, cp_diff);
+
+	mibss.cpuUser = cpu_states[CP_USER] / 10;
+	mibss.cpuSystem = (cpu_states[CP_SYS] + cpu_states[CP_INTR]) / 10;
+	mibss.cpuIdle = cpu_states[CP_IDLE] / 10;
+	mibss.cpuRawUser = cp_time[CP_USER];
+	mibss.cpuRawNice = cp_time[CP_NICE];
+	mibss.cpuRawSystem = cp_time[CP_SYS] + cp_time[CP_INTR];
+	mibss.cpuRawIdle = cp_time[CP_IDLE];
+	mibss.cpuRawKernel = cp_time[CP_SYS];
+	mibss.cpuRawInterrupt = cp_time[CP_INTR];
+}
+
+/* init all our ss objects */
+
+void
+mibss_init() {
+
+	pagesize = getpagesize();
+
+	mibss.index = 1;
+	mibss.errorName = (const u_char *) "systemStats";
+}
+
+int
+op_systemStats(struct snmp_context *context __unused, struct snmp_value *value, 
+	u_int sub, u_int iidx __unused, enum snmp_op op)
+{
+	int ret;
+	asn_subid_t which = value->var.subs[sub - 1];
+
+	switch (op) {
+
+		case SNMP_OP_GET:
+			break;
+
+		case SNMP_OP_SET:
+			return (SNMP_ERR_NOT_WRITEABLE);
+    
+		case SNMP_OP_GETNEXT:
+		case SNMP_OP_ROLLBACK:
+		case SNMP_OP_COMMIT:
+			return (SNMP_ERR_NOERROR);
+    
+		default:
+			return (SNMP_ERR_RES_UNAVAIL);
+	}
+  	
+	ret = SNMP_ERR_NOERROR;
+
+	switch (which) {
+		
+		case LEAF_memIndex:
+			value->v.integer = mibss.index;
+			break;
+		case LEAF_ssErrorName:
+			ret = string_get(value, mibss.errorName, -1);
+			break;
+		case LEAF_ssSwapIn:
+			value->v.integer = mibss.swapIn;
+			break;
+		case LEAF_ssSwapOut:
+			value->v.integer = mibss.swapOut;
+			break;
+		case LEAF_ssSysInterrupts:
+			value->v.integer = mibss.sysInterrupts;
+			break;
+		case LEAF_ssSysContext:
+			value->v.integer = mibss.sysContext;
+			break;
+		case LEAF_ssCpuUser:
+			value->v.integer = mibss.cpuUser;
+			break;
+		case LEAF_ssCpuSystem:
+			value->v.integer = mibss.cpuSystem;
+			break;
+		case LEAF_ssCpuIdle:
+			value->v.integer = mibss.cpuIdle;
+			break;
+		case LEAF_ssCpuRawUser:
+			value->v.uint32 = mibss.cpuRawUser;
+			break;
+		case LEAF_ssCpuRawNice:
+			value->v.uint32 = mibss.cpuRawNice;
+			break;
+		case LEAF_ssCpuRawSystem:
+			value->v.uint32 = mibss.cpuRawSystem;
+			break;
+		case LEAF_ssCpuRawIdle:
+			value->v.uint32 = mibss.cpuRawIdle;
+			break;
+		case LEAF_ssCpuRawWait:
+			value->v.uint32 = mibss.cpuRawWait;
+			break;
+		case LEAF_ssCpuRawKernel:
+			value->v.uint32 = mibss.cpuRawKernel;
+			break;
+		case LEAF_ssCpuRawInterrupt:
+			value->v.uint32 = mibss.cpuRawInterrupt;
+			break;
+		case LEAF_ssRawInterrupts:
+			value->v.uint32 = mibss.rawInterrupts;
+			break;
+		case LEAF_ssRawContexts:
+			value->v.uint32 = mibss.rawContexts;
+			break;
+		case LEAF_ssCpuRawSoftIRQ:
+			value->v.uint32 = mibss.cpuRawSoftIRQ;
+			break;
+		case LEAF_ssRawSwapIn:
+			value->v.uint32 = mibss.rawSwapIn;
+			break;
+		case LEAF_ssRawSwapOut:
+			value->v.uint32 = mibss.rawSwapOut;
+			break;
+		default:
+			ret = SNMP_ERR_RES_UNAVAIL;
+			break;
+	}
+
+	return (ret);
+};
+
